cc1c28ce69046ea34e9315feb584932aa29e61b8
rabindra12345 <rabindradahal2015@gmail.com>
2026-06-13 18:06:59 +0545

    just committing a author.

--- news_ims/src/main/java/com/pironews/news_ims/NewsImsApplication.java
+++ news_ims/src/main/java/com/pironews/news_ims/NewsImsApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * @ author: Rabindra
+ * **/
 public class NewsImsApplication {
 
     public static void main(String[] args) {
